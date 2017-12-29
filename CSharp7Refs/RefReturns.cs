0ff37258ebb0fe00495fd89f49b8cec373c690e1
Ibro <ibro@ibro.me>
2017-12-29 21:53:00 +0100

    return reference!

---
+++ CSharp7Refs/RefReturns.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp7Refs
+{
+    public class RefReturns
+    {
+        public RefReturns()
+        {
+            this._grade = 2;
+        }
+
+        private int _grade;
+
+        public void PrintGrade()
+        {
+            Console.WriteLine($"Grade: {this._grade}");
+        }
+
+        public ref int DoStuff()
+        {
+            return ref this._grade;
+        }
+    }
+}
