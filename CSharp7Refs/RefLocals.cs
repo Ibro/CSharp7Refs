d2c2f39bd686ab1bdabadd972b75d9666eba899e
Ibro <ibro@ibro.me>
2017-12-29 21:52:51 +0100

    local reference variables

---
+++ CSharp7Refs/RefLocals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp7Refs
+{
+    public class RefLocals
+    {      
+        public static void PrintPoints()
+        {
+            int points = 15;
+
+            ref int refToPoints = ref points;
+
+            refToPoints = 125;
+
+            Console.WriteLine($"Points: {points}");
+        }
+    }
+}
